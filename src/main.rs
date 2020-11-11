4fd36ecafa78c01f09f41912ec43a9746b147ce7
VentGrey <omarpurataf@gmail.com>
2020-11-11 17:28:53 -0600

    Added basic clap handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    App::new("charlatan")
+        .version("0.1.0")
+        .about("Unnoficial Julia toolchain installer")
+        .long_about(
+            "A small program written in Rust that I made to avoid using
+            external bash scripts and manage easily julia versions and
+            toolkits.
+            ")
+        .author("VentGrey")
+        .bin_name("charlatan")
+        .get_matches();
 }
