a7acf40c0c1adfa49a22b7608048b561ae2f463b
VentGrey <omarpurataf@gmail.com>
2020-11-15 17:53:23 -0600

    Different approach on subcommands this time

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,9 @@ use clap::{App, Arg, SubCommand};
 fn main() {
     let matches = App::new("charlatan")
         .subcommand(
-            SubCommand::with_name("self")
-                .about("Used to handle charlatan itself")
-                .arg(
-                    Arg::with_name("action")
-                        .help(
-                            "Can be either:
-                        update: Downloads the latest charlatan binary
-                        remove / uninstall: Uninstalls charlatan",
-                        )
-                        .index(1)
-                        .required(true),
-                ),
+                App::new("self")
+                        .about("manage charlatan itself")
+                        .arg(Arg::with_name("update").help("update charlatan"))
         )
         .version("0.1.0")
         .about("Unnoficial Julia toolchain installer")
