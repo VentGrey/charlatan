19e008f2e8492d6d8a51cbb1b08e369f912cf6c0
VentGrey <omarpurataf@gmail.com>
2020-11-14 20:54:25 -0600

    Added self subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use clap::App;
+use clap::{App, Arg, SubCommand};
 
 fn main() {
-    App::new("charlatan")
+    let matches = App::new("charlatan")
+        .subcommand(
+            SubCommand::with_name("self")
+                .about("Used to handle charlatan itself")
+                .arg(
+                    Arg::with_name("action")
+                        .help(
+                            "Can be either:
+                        update: Downloads the latest charlatan binary
+                        remove / uninstall: Uninstalls charlatan",
+                        )
+                        .index(1)
+                        .required(true),
+                ),
+        )
         .version("0.1.0")
         .about("Unnoficial Julia toolchain installer")
         .long_about(
             "A small program written in Rust that I made to avoid using
-            external bash scripts and manage easily julia versions and
-            toolkits.
-            ")
+                external bash scripts and manage easily julia versions and
+                toolkits.
+                ",
+        )
         .author("VentGrey")
         .bin_name("charlatan")
         .get_matches();
