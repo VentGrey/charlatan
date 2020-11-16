80775245c2b3027386a0f86918c607bc233f8e2f
VentGrey <omarpurataf@gmail.com>
2020-11-15 22:57:02 -0600

    Done experimenting with args

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,19 @@
+use std::env;
+
 fn main() {
+        let args: Vec<String> = env::args().collect();
+        let option = &args[1];
+
+        if args.len() > 2 {
+                panic!("excess arguments!");
+        }
+
+        match option.as_str() {
+                "install" => println!("Installing Julia Toolchain"),
+                "update" => println!("Updating julia toolchain"),
+                _ => println!("wtf am I supposed to do?")
+        }
+
+        println!("First option: {:?}", option);
+        println!("{:?}", args);
 }
