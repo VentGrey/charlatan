80c79a05b473f7c229d02746017f56aacb313c13
VentGrey <omarpurataf@gmail.com>
2020-11-15 18:40:54 -0600

    Parse arguments the 'old' way. Clap is shit

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,2 @@
-use clap::{App, Arg};
-
 fn main() {
-    let matches = App::new("charlatan")
-        .subcommand(
-            App::new("self")
-                .about("manage charlatan itself")
-                .arg(Arg::with_name("update").help("update charlatan"))
-                .arg(Arg::with_name("remove").help("uninstalls charlatan"))
-                .arg(Arg::with_name("uninstall").help("uninstalls charlatan")),
-        )
-        .arg(Arg::with_name("update").help("updates your currenr julia install"))
-        .version("0.1.0")
-        .about("Unnoficial Julia toolchain installer")
-        .long_about(
-            "A small program written in Rust that I made to avoid using
-                external bash scripts and manage easily julia versions and
-                toolkits.
-                ",
-        )
-        .author("VentGrey")
-        .bin_name("charlatan")
-        .get_matches();
 }
