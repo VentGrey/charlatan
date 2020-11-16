1d84ec333c06f7202a3fb352211640cb4b8428f6
VentGrey <omarpurataf@gmail.com>
2020-11-16 00:02:59 -0600

    Unrecoverable errors from argument misuse

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,25 @@ use std::process::exit;
 
 fn main() {
         let args: Vec<String> = env::args().collect();
-        let option = &args[1];
 
         if args.len() > 2 {
                 println!("Excess of arguments!");
                 exit(1);
-        }
-
-        match option.as_str() {
-                "install" => println!("Installing Julia Toolchain"),
-                "update" => println!("Updating julia toolchain"),
-                _ => {
-                        println!("Invalid argument...");
-                        exit(1);
+        } else if args.len() == 1 {
+                println!("Empty arguments list!");
+                exit(1);
+        } else if args.is_empty() {
+                println!("Empty argument list");
+                exit(1);
+        } else {
+                let option = &args[1];
+                match option.as_str() {
+                        "install" => println!("Installing Julia Toolchain"),
+                        "update"  => println!("Updating julia toolchain"),
+                        _ => {
+                                println!("Invalid argument...");
+                                exit(1);
+                        }
                 }
         }
-
-        println!("First option: {:?}", option);
-        println!("{:?}", args);
 }
