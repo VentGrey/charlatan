adce6161c5e288e2143ee6bc6faa601082e2bdef
VentGrey <omarpurataf@gmail.com>
2020-11-18 17:11:32 -0600

    Started working on compatibility thingies

--- src/install.rs
+++ src/install.rs
@@ -3,7 +3,21 @@ use std::process::Command;
 // External Crates
 use colored::*;
 
+fn compatible() -> bool {
+    let compat: bool = if cfg!(target_os = "windows") {
+        eprintln!("{}", "Sorry Windows isn't compatible, it has an installer already");
+        false
+    } else if cfg!(target_os = "linux") {
+        println!("You seem to be running {}...", "algo");
+        true
+    } else {
+        eprintln!("Could not determine compatibility with your OS. Sorry");
+        false
+    };
 
-pub fn entry_point() {
+    compat
+}
+
+pub fn download() {
 
 }
