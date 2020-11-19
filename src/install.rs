3f0f16b2fd1d89ce6d96ec2f2a877b96a08cae36
VentGrey <omarpurataf@gmail.com>
2020-11-18 19:24:19 -0600

    Added a dependency message over wget

--- src/install.rs
+++ src/install.rs
@@ -5,9 +5,22 @@ use colored::*;
 
 fn compatible() -> bool {
     let compat: bool = if cfg!(target_os = "windows") {
-        eprintln!("{}", "Sorry Windows isn't compatible, it has an installer already");
+        eprintln!(
+            "{}",
+            "Sorry Windows isn't compatible, it has an installer already"
+        );
         false
     } else if cfg!(target_os = "linux") {
+        // Check if wget is installed on your system
+        let is_wget = Command::new("which")
+            .arg("wget")
+            .status()
+            .expect("Could not run which");
+
+        if is_wget.code() != Some(0) {
+            println!("Charlatan needs wget to be installed")
+        }
+
         println!("You seem to be running {}...", "algo");
         true
     } else {
@@ -19,5 +32,7 @@ fn compatible() -> bool {
 }
 
 pub fn download() {
+    println!("Determining system compatibility...");
 
+    let is_compat = compatible();
 }
--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,30 @@ use std::process::exit;
 // Mod usage
 mod install;
 
-fn main() {
-        let args: Vec<String> = env::args().collect();
+// External crates
+use colored::*;
 
-        if args.len() > 2 {
-                println!("Excess of arguments!");
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        println!("Excess of arguments!");
+        exit(1);
+    } else if args.len() == 1 {
+        println!("Empty arguments list!");
+        exit(1);
+    } else if args.is_empty() {
+        println!("Empty argument list!");
+        exit(1);
+    } else {
+        let option = &args[1];
+        match option.as_str() {
+            "install" => install::download(),
+            "update" => println!("Updating julia toolchain"),
+            _ => {
+                println!("Invalid argument...");
                 exit(1);
-        } else if args.len() == 1 {
-                println!("Empty arguments list!");
-                exit(1);
-        } else if args.is_empty() {
-                println!("Empty argument list!");
-                exit(1);
-        } else {
-                let option = &args[1];
-                match option.as_str() {
-                        "install" => install::entry_point(),
-                        "update"  => println!("Updating julia toolchain"),
-                        _ => {
-                                println!("Invalid argument...");
-                                exit(1);
-                        }
-                }
+            }
         }
+    }
 }
