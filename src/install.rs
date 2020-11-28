7c6445ec73423a6211e51f09b7dc02e8572e53aa
VentGrey <omarpurataf@gmail.com>
2020-11-28 04:34:33 -0600

    Added current version downloads into the program onto /tmp dir

--- src/install.rs
+++ src/install.rs
@@ -55,15 +55,36 @@ fn download(ver: i32) {
                 "Downloading latest Julia Stable tarball to {}",
                 "/tmp".blue()
             );
+
+            Command::new("wget")
+                .arg("https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.3-linux-x86_64.tar.gz")
+                .arg("-P")
+                .arg("/tmp")
+                .status()
+                .expect("could not run wget!");
         }
         2 => {
             println!(
                 "Downloading latest Julia Nightly tarball to {}",
                 "/tmp".blue()
             );
+
+            Command::new("wget")
+                .arg("https://julialangnightlies-s3.julialang.org/bin/linux/x64/julia-latest-linux64.tar.gz")
+                .arg("-P")
+                .arg("/tmp")
+                .status()
+                .expect("could not run wget!");
         }
         3 => {
             println!("Downloading latest Julia LTS tarball to {}", "/tmp".blue());
+
+            Command::new("wget")
+                .arg("https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.5-linux-x86_64.tar.gz")
+                .arg("-P")
+                .arg("/tmp")
+                .status()
+                .expect("could not run wget!");
         }
         _ => {
             eprintln!("Unknown option {}", ver);
