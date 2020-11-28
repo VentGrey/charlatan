be64ea791687f935ecff5084a7a4340ad7033a0c
VentGrey <omarpurataf@gmail.com>
2020-11-28 04:26:07 -0600

    Added help function into main file

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod install;
 // External crates
 use colored::*;
 
+/** Main crate function.
+The following function can handle arguments in a small order */
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -19,12 +21,41 @@ fn main() {
     } else {
         let option = &args[1];
         match option.as_str() {
-            "install" => install::install(),
+            "install" => install::init(),
             "update" => println!("Updating julia toolchain"),
+            "help" => help(),
             _ => {
                 println!("Invalid argument...");
                 exit(1);
             }
         }
     }
+}
+
+fn help() {
+    const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+    const AUTHORS: Option<&'static str> = option_env!("CARGO_PKG_AUTHORS");
+
+    println!(
+        "Charlatan ({})
+A Rust program used to ironically install Julia
+
+Authors: [{}]
+License: [GPL-2.0]
+
+USAGE:
+    charlatan [ARGS]
+
+ARGS:
+    install    Installs a selected Julia Toolchain
+    update     Updated the existing Julia Toolchain
+    help       Displays this message
+
+For more information about the development of this program please see
+the README file included in:
+https://github.com/VentGrey/charlatan/blob/master/README.md
+",
+        VERSION.unwrap_or("unknown"),
+        AUTHORS.unwrap_or("none"),
+    );
 }
